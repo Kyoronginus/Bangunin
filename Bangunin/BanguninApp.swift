ead0849d8be7ac578ab3ce8bffab622ff45f041e
Tohru <tohrudj0711@gmail.com>
2026-07-02 08:47:19 +0700

    Initial Commit

---
+++ Bangunin/BanguninApp.swift
@@ -0,0 +1,17 @@
+//
+//  BanguninApp.swift
+//  Bangunin
+//
+//  Created by Tohru Djunaedi Sato on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct BanguninApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
