8fcce5fc0512b6487febab627460e95e2e5b5a2e
Tohru <tohrudj0711@gmail.com>
2026-07-12 02:33:58 +0700

    fix: optimize station selection logic to decrease unnecessary computation

--- Bangunin/AppiOS/ViewModels/StationSelectionViewModel.swift
+++ Bangunin/AppiOS/ViewModels/StationSelectionViewModel.swift
@@ -10,42 +10,64 @@ import Foundation
 @Observable
 class StationSelectionViewModel {
     var stations: [Station]
-    var indexedStations: [String: [Station]] = [:]
+    var indexedStations: [(String, [Station])] = []
     var searchText: String = ""
-
+    
     init(stations: [Station]) {
         self.stations = stations
-        indexStation()
-    }
-
-    var filteredStations: [Station] {
-        if searchText.isEmpty {
-            return stations
-        } else {
-            return stations.filter {
-                $0.name.localizedStandardContains(searchText)
-            }
-        }
+        let groupedStations = Dictionary(grouping: stations, by: { $0.name.first!.uppercased() })
+        
+        self.indexedStations = groupedStations.map { index, stationName in
+            (index, stationName.sorted(by: { $0.name < $1.name }))
+        }.sorted(by: { $0.0 < $1.0 })
     }
     
-    func indexStation() {
-        for station in stations {
-            if let index = station.name.first?.uppercased() {
-                if indexedStations[index] == nil {
-                    indexedStations[index] = [station]
+//    var filteredStations: [Station] {
+//        if searchText.isEmpty {
+//            return stations
+//        } else {
+//            return stations.filter {
+//                $0.name.localizedStandardContains(searchText)
+//            }
+//        }
+//    }
+    
+    var filteredIndexedStations: [(String, [Station])] {
+        if searchText.isEmpty {
+            return indexedStations
+        } else {
+            return indexedStations.compactMap { index, stations in
+                let filtered = stations.filter {
+                    $0.name.localizedStandardContains(searchText)
                 }
-                else {
-                    indexedStations[index]?.append(station)
-                }
+                return filtered.isEmpty ? nil : (index, filtered)
             }
         }
+    }
+}
+    
+//    func indexStation() {
+//        for station in stations {
+//            if let index = station.name.first?.uppercased() {
+//                if indexedStations[index] == nil {
+//                    indexedStations[index] = [station]
+//                }
+//                else {
+//                    indexedStations[index]?.append(station)
+//                }
+//            }
+//        }
+//    
+//    func indexStation() {
+//        return Dictionary(grouping: filteredStations, by: {})
+//    }
         
         // Sort the dictionary.
 //        for (index, stations) in indexedStations {
 //            let sorted = stations.sorted{ $0.name.localizedCapitalized < $1.name.localizedCapitalized }
 //            // Masukin balik
 //        }
-    }
+//    }
 //    var groupedStations: [(String, [Station])] {
 //        let grouped = Dictionary(grouping: filteredStations) { station in
 //            String(station.name.prefix(1)).uppercased()
@@ -55,4 +77,3 @@ class StationSelectionViewModel {
 //        }
 //        .sorted { $0.0 < $1.0 }
 //    }
-}
--- Bangunin/AppiOS/Views/UnifiedStationSelectionView.swift
+++ Bangunin/AppiOS/Views/UnifiedStationSelectionView.swift
@@ -26,32 +26,62 @@ struct UnifiedStationSelectionView: View {
     var body: some View {
         VStack(spacing: 0) {
             List {
-                if viewModel.searchText.isEmpty {
-                    Section {
-                        EmptyView()
-                    } header: {
-                        EmptyView()
-                    }
-                    .sectionIndexLabel("X")
+                
+                
 
-//                    ForEach(viewModel.groupedStations, id: \.0) { group in
-//                        // If []
-//                        Section(header: Text(group.0)) {
-//                            ForEach(group.1, id: \.name) { station in
-//                                stationRow(for: station)
-//                            }
-//                        }
-//                        .sectionIndexLabel(group.0)
-//                    }
-                } else {
-                    ForEach(
-                        viewModel.filteredStations.sorted(by: {
-                            $0.name < $1.name
-                        }),
-                        id: \.name
-                    ) { station in
-                        stationRow(for: station)
+                //                    Section {
+                //                        EmptyView()
+                //                    } header: {
+                //                        EmptyView()
+                //                    }
+                //                    .sectionIndexLabel("X")
+                //
+                ////                    ForEach(viewModel.groupedStations, id: \.0) { group in
+                ////                        // If []
+                ////                        Section(header: Text(group.0)) {
+                ////                            ForEach(group.1, id: \.name) { station in
+                ////                                stationRow(for: station)
+                ////                            }
+                ////                        }
+                ////                        .sectionIndexLabel(group.0)
+                ////                    }
+                //
+                //                    ForEach(viewModel.filteredIndexedStation, id: \.0) { station in
+                //
+                //                        let index = station.0
+                //                        let stations = station.1
+                //
+                //                        Section(header: Text(index)) {
+                //                            ForEach(stations, id: \.name) { station in
+                //                                stationRow(for: station)
+                //                            }
+                //                        }
+                //                    }
+                //
+                //
+                //                } else {
+                ////                    ForEach(
+                ////                        viewModel.filteredStations.sorted(by: {
+                ////                            $0.name < $1.name
+                ////                        }),
+                ////                        id: \.name
+                ////                    ) { station in
+                ////                        stationRow(for: station)
+                ////                    }
+                //                    ForEach(viewModel.indexedStations, id: \.self) { station in
+                //                        stationRow(for: station.0)
+                //                    }
+
+                ForEach(viewModel.filteredIndexedStations, id: \.0) { group in
+                    let index = group.0
+                    let stationsInGroup = group.1
+                    
+                    Section(header: Text(index)) {
+                        ForEach(stationsInGroup, id: \.name) { station in
+                            stationRow(for: station)
+                        }
                     }
+                    .sectionIndexLabel(index)
                 }
             }
             .listStyle(.plain)
