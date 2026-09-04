92ad10ff440ef14f58839a83ad489da3314246d8
Tohru <tohrudj0711@gmail.com>
2026-09-04 11:37:39 +0700

    feat: add Unit tests and create workflow

---
+++ BanguninTests/AddAlarmViewModelTests.swift
@@ -0,0 +1,19 @@
+//
+//  BanguninTests.swift
+//  BanguninTests
+//
+//  Created by Tohru Djunaedi Sato on 04/09/26.
+//
+
+import Testing
+@testable import Bangunin
+
+struct BanguninTests {
+    
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
